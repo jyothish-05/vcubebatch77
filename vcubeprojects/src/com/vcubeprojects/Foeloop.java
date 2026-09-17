b24d741c9742e1048d96b88d2cc50939dfeeaf45
Jyothish <maddinenijyothish37@gmail.com>
2026-09-17 09:33:11 +0530

    for loop no from 1 to 20

---
+++ vcubeprojects/src/com/vcubeprojects/Foeloop.java
@@ -0,0 +1,15 @@
+package com.vcubeprojects;
+
+public class Foeloop {
+
+	public static void main(String[] args) {
+		//for(int i=1;i<=20;i++) { 
+			//System.out.println(i);
+		//}
+		for(int i = 0;i<=50;i = i+2) {
+			System.out.println(i);
+		}
+
+	}
+
+}
