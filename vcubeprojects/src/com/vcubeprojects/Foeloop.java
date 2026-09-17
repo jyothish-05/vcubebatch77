f00c35acc3fa1c4b970fd566884eabd42df364cd
Jyothish <maddinenijyothish37@gmail.com>
2026-09-17 09:45:45 +0530

    numbers from 1 to 50 using for loop

--- vcubeprojects/src/com/vcubeprojects/Foeloop.java
+++ vcubeprojects/src/com/vcubeprojects/Foeloop.java
@@ -3,10 +3,7 @@ package com.vcubeprojects;
 public class Foeloop {
 
 	public static void main(String[] args) {
-		//for(int i=1;i<=20;i++) { 
-			//System.out.println(i);
-		//}
-		for(int i = 0;i<=50;i = i+2) {
+		for(int i=1;i<=20;i++) { 
 			System.out.println(i);
 		}
 
